c81cd14b661d389fc10d7e39558cab9d83687388
Karthik <karthiknair2004@gmail.com>
2025-08-30 01:39:08 +0530

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import Hero from "./sections/Hero.jsx";
 
 const App = () => {
   return (
-    <main>
-        <div>
-            <h1>Welcome to my Portfolio!</h1>
-        </div>
-    </main>
-  )
-}
+    <>
+      <Hero />
+    </>
+  );
+};
 
 export default App;
