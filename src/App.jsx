5f7ef4809d9ddd01cdd5757e07095f9afa942ea1
Karthik <karthiknair2004@gmail.com>
2025-09-09 20:33:45 +0530

    Work Experience Section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import FeatureCards from "./sections/FeatureCards.jsx";
 import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import Experience from "./sections/Experience.jsx";
+import TechStack from "./sections/TechStack.jsx";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       {/*<LogoSection /> */}
       <FeatureCards />
       <Experience />
+      <TechStack />
     </>
   );
 };
